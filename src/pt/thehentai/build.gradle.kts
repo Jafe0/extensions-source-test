932489cf810876644161deae67e1275f94977ee4
Jafe0 <expeditoromulo1555@gmail.com>
2025-04-19 11:47:51 -0300

    Update build.gradle.kts

--- src/pt/thehentai/build.gradle.kts
+++ src/pt/thehentai/build.gradle.kts
@@ -1,26 +1,11 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "pt.thehentai"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 34
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+    id("tachiyomi.extension")
 }
 
 dependencies {
     implementation(project(":lib:synchrony"))
+
+    // bibliotecas padrões de extensões
+    implementation(libs.jsoup)
+    implementation(libs.okhttp)
 }

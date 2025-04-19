a5855be690e1513fb3dee34cda34a7c9eede6bed
Jafe0 <expeditoromulo1555@gmail.com>
2025-04-19 10:13:39 -0300

    Delete src/pt/thehentai/TheHentai.kt

--- src/pt/thehentai/TheHentai.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.pt.thehentai
-
-import eu.kanade.tachiyomi.multisrc.synchrony.Synchrony
-
-class TheHentai : Synchrony(
-    "TheHentai",
-    "https://thehentai.world",
-    "pt",
-)

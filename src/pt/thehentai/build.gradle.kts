06f036c2d2120fae9464936101d498843d9d1cf8
Jafe0 <expeditoromulo1555@gmail.com>
2025-04-19 09:49:02 -0300

    Update build.gradle.kts

--- src/pt/thehentai/build.gradle.kts
+++ src/pt/thehentai/build.gradle.kts
@@ -1,12 +1,3 @@
 plugins {
     id("tachiyomi.extension")
 }
-
-dependencies {
-    // Dependências específicas da sua extensão
-    implementation(project(":lib:synchrony"))
-    
-    // Se no futuro precisar de mais libs, pode adicionar aqui
-    // exemplo:
-    // implementation(project(":lib:some-other-lib"))
-}

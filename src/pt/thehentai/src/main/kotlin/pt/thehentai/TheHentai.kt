bf7ff81cfaf8b2d151146fef03eb911529bc819f
Jafe0 <expeditoromulo1555@gmail.com>
2025-04-19 10:11:54 -0300

    Create TheHentai.kt

---
+++ src/pt/thehentai/src/main/kotlin/pt/thehentai/TheHentai.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.pt.thehentai
+
+import eu.kanade.tachiyomi.multisrc.synchrony.Synchrony
+
+class TheHentai : Synchrony(
+    "TheHentai",
+    "https://thehentai.world",
+    "pt",
+)
